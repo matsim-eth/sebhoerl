8a4bf5c51b0be245ae80792d3d2feeb9cd139867
Sebastian Hörl <hoerl.sebastian@gmail.com>
2017-10-26 10:27:30 +0200

    Time shifting by plan or person

--- src/main/java/ch/ethz/ivt/matsim/playgrounds/sebhoerl/utils/ShiftTimes.java
+++ src/main/java/ch/ethz/ivt/matsim/playgrounds/sebhoerl/utils/ShiftTimes.java
@@ -13,28 +13,37 @@ import java.util.stream.Collectors;
 public class ShiftTimes {
 	final private double interval;
 	final private Random random;
+	final private boolean shiftPerPlan;
 	
-	public ShiftTimes(double interval, Random random) {
+	public ShiftTimes(double interval, Random random, boolean shiftPerPlan) {
 		this.interval = interval;
 		this.random = random;
+		this.shiftPerPlan = shiftPerPlan;
 	}
 	
     public void apply(Population population) {
         for (Person person : population.getPersons().values()) {
-            Plan plan = person.getSelectedPlan();
-            List<Activity> activities = person.getSelectedPlan().getPlanElements().stream().filter(p -> p instanceof Activity).map(Activity.class::cast).collect(Collectors.toList());
-
-            double firstEndTime = activities.get(0).getEndTime();
-            double minimumOffset = -firstEndTime;
-            double offset = Math.max(minimumOffset, random.nextDouble() * interval - 0.5 * interval);
-
-            for (PlanElement planElement : plan.getPlanElements()) {
-                if (planElement instanceof Activity) {
-                    Activity activity = (Activity) planElement;
-                    activity.setEndTime(activity.getEndTime() + offset);
-                    activity.setStartTime(activity.getStartTime() + offset);
-                }
-            }
+        	double r = random.nextDouble();
+        	
+        	for (Plan plan : person.getPlans()) {
+        		if (shiftPerPlan) {
+        			r = random.nextDouble();
+        		}
+        		
+	            List<Activity> activities = person.getSelectedPlan().getPlanElements().stream().filter(p -> p instanceof Activity).map(Activity.class::cast).collect(Collectors.toList());
+	
+	            double firstEndTime = activities.get(0).getEndTime();
+	            double minimumOffset = -firstEndTime;
+	            double offset = Math.max(minimumOffset, r * interval - 0.5 * interval);
+	
+	            for (PlanElement planElement : plan.getPlanElements()) {
+	                if (planElement instanceof Activity) {
+	                    Activity activity = (Activity) planElement;
+	                    activity.setEndTime(activity.getEndTime() + offset);
+	                    activity.setStartTime(activity.getStartTime() + offset);
+	                }
+	            }
+        	}
         }
     }
     
@@ -43,7 +52,7 @@ public class ShiftTimes {
         new PopulationReader(scenario).readFile(args[0]);
         
         double interval = args.length > 2 ? Double.parseDouble(args[2]) : 1200.0;
-        new ShiftTimes(interval, new Random()).apply(scenario.getPopulation());
+        new ShiftTimes(interval, new Random(), false).apply(scenario.getPopulation());
         
         new PopulationWriter(scenario.getPopulation()).write(args[1]);
     }
